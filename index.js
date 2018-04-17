d94f877b1f860ba74cf725f486a4b84ba0f1df8d
jaredianmills <jaredianmills@gmail.com>
2018-04-17 03:58:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function nowServing(katzDeli) {
 
 function currentLine(katzDeli) {
   var isCurrently = "The line is currently:"
-  var currentOrder = []
+  var currentOrder = {}
   if (katzDeli.length === 0) {
     return "The line is currently empty"
   } else {
