8b6e795cac7ba5b5c67ae59a2e0ba2ec543b3cc5
jaredianmills <jaredianmills@gmail.com>
2018-04-17 04:17:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function nowServing(katzDeli) {
 function currentLine(katzDeli) {
   var currentOrder = []
   if (katzDeli.length === 0) {
-    return "The line is currently empty"
+    return "The line is currently empty."
   } else {
     for (let i = 0; i < katzDeli.length; i++) {
       var orderNumber = i + 1
